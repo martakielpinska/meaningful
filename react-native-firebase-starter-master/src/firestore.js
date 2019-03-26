8ac987a27fffdb5114b1af694e4a3bd6f16b207f
Marta K <m.kielpinska@student.fontys.nl>
2019-03-26 13:13:37 +0100

    FRESH APP

--- react-native-firebase-starter-master/src/firestore.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'react-native-firebase'
-class FirebaseService {
-  constructor() {
-    this.ref = firebase.firestore().collection('users')
-  }
-  
-}
-export const firebaseService = new FirebaseService()
